f9f4802e3006069b29cbef4a21c3b6f93e97bfb2
test <test@ren>
2024-12-05 11:13:59 +0800

    重现 Druid 连接池启用公平锁(配置 maxWait 后)场景下，当并发较高会导致从连接池中取还连接较为耗时，进而触发用户感知慢查询然后重试导致雪崩 https://github.com/alibaba/druid/wiki/DruidDataSource%E9%85%8D%E7%BD%AE%E5%B1%9E%E6%80%A7%E5%88%97%E8%A1%A8

--- src/main/java/HelloDruid.java
+++ src/main/java/HelloDruid.java
@@ -5,53 +5,67 @@ import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class HelloWorldDruid {
+public class HelloDruid {
     private static DruidDataSource dataSource;
 
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Usage: java DruidThreadTest <ip> <port>");
+        if (args.length != 5) {
+            System.out.println("Usage: java DruidThreadTest <ip> <port> fairLock threadNum connNum");
             System.exit(1);
         }
 
         String ip = args[0];
         String port = args[1];
+        int threadNum = Integer.parseInt(args[3]);
+        int connNum = Integer.parseInt(args[4]);
+        boolean fair = Boolean.parseBoolean(args[2]);
 
-        initDataSource(ip, port);
+        initDataSource(ip, port, connNum, fair);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(100);
+        ExecutorService executorService = Executors.newFixedThreadPool(threadNum);
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < threadNum; i++) {
             executorService.submit(new DatabaseTask());
         }
 
         executorService.shutdown();
     }
 
-    private static void initDataSource(String ip, String port) {
+    private static void initDataSource(String ip, String port, int connNum, boolean fair) {
         dataSource = new DruidDataSource();
-        dataSource.setUrl("jdbc:mysql://" + ip + ":" + port + "/test?useSSL=false&connectTimeout=1500&socketTimeout=1700");
+        dataSource.setUrl("jdbc:mysql://" + ip + ":" + port + "/sbtest?useSSL=false&connectTimeout=1500&socketTimeout=1700");
         dataSource.setUsername("root");
         dataSource.setPassword("123");
-        dataSource.setInitialSize(0);
+        dataSource.setInitialSize(connNum);
         dataSource.setMinIdle(0);
-        dataSource.setMaxActive(1);
+        dataSource.setMaxActive(connNum);
+        //dataSource.setMaxWait(60000);
+        dataSource.setUseUnfairLock(!fair);
+        if(fair){
+            dataSource.setMaxWait(60000);
+        }
+
     }
 
     static class DatabaseTask implements Runnable {
         @Override
         public void run() {
+
             while (true) {
+                long iter=100000;
                 try (Connection conn = dataSource.getConnection()) {
                     Statement stmt = conn.createStatement();
                      //PreparedStatement stmt = conn.prepareStatement("SELECT 1")) {
 
-                    stmt.execute("SELECT sleep(0.005), id from sbtest1 limit 1;");
-                    Thread.sleep(1);
-
+                    stmt.execute("SELECT sleep(0.001), id from sbtest1 limit 1;");
+                    //Thread.sleep(1);
+                    while(iter>0){
+                        iter--;
+                    }
+                    stmt.close();
                     //System.out.println("id:"+Thread.currentThread().toString());
                     //System.out.println("dataSource:"+dataSource.getActiveCount());
-                } catch (SQLException | InterruptedException e) {
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
