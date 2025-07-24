5cef2a702f273fd52c5ae1f6df3238359dcda222
test <test@ren>
2025-07-24 11:42:02 +0800

    hello druid example

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -classpath ./helloworld-1.0.jar HelloWorldDruid 127.0.0.1 3306
+//参数说明：第三个参数 true 表示启用公平锁，1000 表示 1000 个并发，100 表示 Druid 数据库连接数 100
+taskset -a -c 0-15,32-47 java -jar /root/HelloDruid/hellodruid-1.0.jar 127.1 3306 true 1000 100
+
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -classpath ./helloworld-1.0.jar HelloDruid 127.0.0.1 3306
 
 java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005  -classpath ./helloworld-1.0.jar JedisTest 127.0.0.1 6379
 
--- src/main/java/HelloDruid.java
+++ src/main/java/HelloDruid.java
@@ -1,5 +1,7 @@
 import com.alibaba.druid.pool.DruidDataSource;
+import java.sql.*;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.SQLException;
 import java.util.concurrent.ExecutorService;
@@ -33,16 +35,29 @@ public class HelloDruid {
 
     private static void initDataSource(String ip, String port, int connNum, boolean fair) {
         dataSource = new DruidDataSource();
-        dataSource.setUrl("jdbc:mysql://" + ip + ":" + port + "/sbtest?useSSL=false&connectTimeout=1500&socketTimeout=1700");
+        dataSource.setUrl("jdbc:mysql://" + ip + ":" + port + "/sbtest?useSSL=false&connectTimeout=15000&socketTimeout=17000&useServerPrepStmts=true&cachePrepStmts=true");
         dataSource.setUsername("root");
         dataSource.setPassword("123");
         dataSource.setInitialSize(connNum);
         dataSource.setMinIdle(0);
         dataSource.setMaxActive(connNum);
+        dataSource.setMinEvictableIdleTimeMillis(1);
+        dataSource.setTimeBetweenEvictionRunsMillis(3000);
         //dataSource.setMaxWait(60000);
+        dataSource.setMaxWait(60000000);
         dataSource.setUseUnfairLock(!fair);
         if(fair){
-            dataSource.setMaxWait(60000);
+            dataSource.setMaxWait(60000000);
+        }
+        try {
+            dataSource.init();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
 
     }
@@ -51,18 +66,50 @@ public class HelloDruid {
         @Override
         public void run() {
 
+            PreparedStatement updateStmt = null;
+            Statement stmt = null;
             while (true) {
                 long iter=100000;
                 try (Connection conn = dataSource.getConnection()) {
-                    Statement stmt = conn.createStatement();
+                    conn.setAutoCommit(false);
+                    stmt = conn.createStatement();
                      //PreparedStatement stmt = conn.prepareStatement("SELECT 1")) {
+                    //stmt.executeQuery()
 
-                    stmt.execute("SELECT sleep(0.001), id from sbtest1 limit 1;");
-                    //Thread.sleep(1);
-                    while(iter>0){
-                        iter--;
+                    ResultSet rs = stmt.executeQuery("select sleep(0.046) from sbtest1 limit 1");
+
+                    /*
+                    ResultSet rs = stmt.executeQuery("SELECT k from ren where id=1 for update;");
+                    if(rs.next()){
+                        long currentK = rs.getLong("k");
+                        currentK++;
+                        String updateSQL = "UPDATE ren SET k = ? WHERE id = 1";
+                        updateStmt = conn.prepareStatement(updateSQL);
+                        updateStmt.setLong(1, ++currentK);
+                        boolean rs2 = updateStmt.execute();
+                        //ResultSet rs2 = conn.createStatement().executeQuery(updateSQL);
+                        //conn.pre
+                        if (!rs2) {
+                            System.out.println("更新成功！k 从 " + (currentK-1) + " 更新为 " + currentK);
+                            // 提交事务
+                            conn.commit();
+                        } else {
+                            System.out.println("更新失败！");
+                            // 回滚事务
+                            conn.rollback();
+                        }
+                        updateStmt.close();
+                    } */
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
                     }
+//                    while (iter > 0) {
+//                        iter--;
+//                    }
                     stmt.close();
+
                     //System.out.println("id:"+Thread.currentThread().toString());
                     //System.out.println("dataSource:"+dataSource.getActiveCount());
                 } catch (SQLException e) {
